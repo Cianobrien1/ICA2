86058d570b2fbc8935c226dbd3d206b049a51c6a
B221734-2022 <email_not_provided>
2022-11-19 17:07:08 +0000

    fully working user_input esearch

--- user_input_2.py
+++ user_input_2.py
@@ -2,17 +2,23 @@
 import os
 import sys
 import subprocess
+path = "esearch_output"
+os.system("rm -fr ~/ICA2/esearch_output")
 os.system("mkdir -p ~/ICA2/esearch_output")
-os.system("cd ~/ICA2/esearch_output")
-before_func = os.listdir()
-def esearch_input (protein_name_in = input('Enter protein name: '), taxonID = input('Enter taxon ID: ')) :
-   protein_name_arg = protein_name_in.replace(" ", "_")
-   esearch_cmd =  f"esearch -db protein -query '{protein_name_in}[PROTEIN]' | efilter -query txid'{taxonID}'[ORGANISM] | efetch -format fasta > {protein_name_arg}_{taxonID}.fasta"
-   os.system(esearch_cmd)
-   output = print(protein_name_arg+"_"+taxonID+".fasta saved")
-   return output 
+os.chdir(path)
+def esearch_input () :
+    protein_name_in= input('Enter protein name: ')
+    taxonID= input('Enter taxon ID: ')
+    protein_name_arg = protein_name_in.replace(" ", "_")
+    esearch_cmd =  f"esearch -db protein -query '{protein_name_in}[PROTEIN]' 2>/dev/null | efilter -query txid'{taxonID}'[ORGANISM] 2>/dev/null | efetch -format fasta 2>/dev/null > ~/ICA2/esearch_output/{protein_name_arg}_{taxonID}.fasta"
+    os.system(esearch_cmd)
+    return 
 esearch_input()
-after_func = os.listdir()
-if after_func != before_func:
+dir_content = os.listdir()
+output_file_name = dir_content[0]
+if os.stat(output_file_name).st_size == 0:
+    os.system('rm -fr {output_file_name}')
+    print('Function input invalid, please try again')
     esearch_input()
-
+else:
+    print("Fasta file has been saved in esearch_output directory as "+output_file_name)
