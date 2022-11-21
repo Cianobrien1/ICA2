e70bed15868512852a91fd0e93f1781322cecb2f
B221734-2022 <email_not_provided>
2022-11-21 19:52:22 +0000

    updated user input plotcon

--- plotcon.py
+++ plotcon.py
@@ -14,12 +14,13 @@ plotcon_input1 = f"plotcon -sequences {plotcon_arg1} -graph png -winsize 4 -gout
 plotcon_input2 = f"plotcon -sequences {plotcon_arg1} -graph x11 -winsize 4 -goutfile {plotcon_arg2} >/dev/null"
 os.system(plotcon_input1) 
 print('PNG of plot saved to plotcon_output directory')
-def user_input(question= 'Do you want to view the graph now?'):
+def user_input(question= 'Do you want to view the plot now?'):
     reply = str(input(question+' (y/n): ')).lower().strip()
     if reply[0] == 'y':
         os.system(plotcon_input2)
         return True
     if reply[0] == 'n':
+        print('To view plot, download the png file from the plotcon_output directory')
         return False
     else:
         return user_input("Invalid response, please try again.")
