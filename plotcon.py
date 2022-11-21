694981f01075c9fde3dc4666b01a95e174083d4c
B221734-2022 <email_not_provided>
2022-11-21 19:50:22 +0000

    takes user input for graph

--- plotcon.py
+++ plotcon.py
@@ -10,5 +10,18 @@ msf_file_name = msf_file_list[0]
 msf_file_name_only = msf_file_name.replace('.fasta.msf', '')
 plotcon_arg1 = path2+'/'+msf_file_name
 plotcon_arg2 = path3+'/'+msf_file_name_only
-plotcon_input = f"plotcon -sequences {plotcon_arg1} -graph png -winsize 4 -goutfile {plotcon_arg2}"
-os.system(plotcon_input) 
+plotcon_input1 = f"plotcon -sequences {plotcon_arg1} -graph png -winsize 4 -goutfile {plotcon_arg2} >/dev/null"
+plotcon_input2 = f"plotcon -sequences {plotcon_arg1} -graph x11 -winsize 4 -goutfile {plotcon_arg2} >/dev/null"
+os.system(plotcon_input1) 
+print('PNG of plot saved to plotcon_output directory')
+def user_input(question= 'Do you want to view the graph now?'):
+    reply = str(input(question+' (y/n): ')).lower().strip()
+    if reply[0] == 'y':
+        os.system(plotcon_input2)
+        return True
+    if reply[0] == 'n':
+        return False
+    else:
+        return user_input("Invalid response, please try again.")
+user_input()
+
